417a5afba63fa8cfb09e3c3632e5973fb579b4c7
Jaiskid <neeraj001jaiswal@gmail.com>
2020-02-25 02:02:41 +0530

    codeupdate

---
+++ consecutive_number.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int N;
+	cin>>N;
+	int ans=0;
+	for(int n=2;n*(n+1)/2<=N;++n){
+		if((N-n*(n+1)/2)%n==0)
+			ans++;
+	}
+	cout<<ans+1;
+}
